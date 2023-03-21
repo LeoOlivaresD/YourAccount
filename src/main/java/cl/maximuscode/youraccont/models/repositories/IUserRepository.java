a58b9693f5c85e92f06376f3a3d9735ec573f009
LeoOlivaresD <cspraword@gmail.com>
2023-03-21 02:38:27 -0300

    Modificacion de IUserRepository Se agrega metodo findByname con el fin de implementarlo en UsersServices

--- src/main/java/cl/maximuscode/youraccont/models/repositories/IUserRepository.java
+++ src/main/java/cl/maximuscode/youraccont/models/repositories/IUserRepository.java
@@ -4,7 +4,10 @@ import cl.maximuscode.youraccont.models.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface IUserRepository extends JpaRepository<Users, Integer> {
+    Users findByName(String name);
 }
