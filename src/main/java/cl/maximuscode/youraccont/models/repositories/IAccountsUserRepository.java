9777e167fc1816fd74ae90124da6d030b7a1c2a7
LeoOlivaresD <cspraword@gmail.com>
2023-03-22 18:26:43 -0300

    Creacion de metodo findByUser_IdUser Se creo este metodo en el repositorio de la entidad AccountsUser con la finalidad de poder buscar el id de un usuario por medio del parametro user_idUser el cual es el atributo que en nuestra entidad AccountsUser que hace referencia a la entidad Users especificamente idUser, que finalemnte nos retornara las listas vinculadas a ese id

--- src/main/java/cl/maximuscode/youraccont/models/repositories/IAccountsUserRepository.java
+++ src/main/java/cl/maximuscode/youraccont/models/repositories/IAccountsUserRepository.java
@@ -1,10 +1,10 @@
 package cl.maximuscode.youraccont.models.repositories;
-
 import cl.maximuscode.youraccont.models.entities.AccountsUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface IAccountsUserRepository extends JpaRepository<AccountsUser,Integer> {
-
+    List<AccountsUser> findByUser_IdUser(Integer user_idUser);
 }
