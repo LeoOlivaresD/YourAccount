b706a2a446ae7ffe7ade8797a48bcbe0b972e14e
LeoOlivaresD <cspraword@gmail.com>
2023-03-22 18:21:28 -0300

    Se factoriza el codigo

--- src/main/java/cl/maximuscode/youraccont/models/repositories/IUserRepository.java
+++ src/main/java/cl/maximuscode/youraccont/models/repositories/IUserRepository.java
@@ -1,10 +1,8 @@
 package cl.maximuscode.youraccont.models.repositories;
-
 import cl.maximuscode.youraccont.models.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 
 @Repository
