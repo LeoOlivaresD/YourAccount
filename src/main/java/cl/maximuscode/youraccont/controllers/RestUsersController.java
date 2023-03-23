feb68559c5fc09bad48c2cb6fb67839e0bcac7c7
LeoOlivaresD <cspraword@gmail.com>
2023-03-23 02:49:31 -0300

    Rest Controller creacion de método create

--- src/main/java/cl/maximuscode/youraccont/controllers/RestUsersController.java
+++ src/main/java/cl/maximuscode/youraccont/controllers/RestUsersController.java
@@ -1,15 +1,23 @@
 package cl.maximuscode.youraccont.controllers;
 
-
+import cl.maximuscode.youraccont.models.entities.Users;
 import cl.maximuscode.youraccont.models.service.UsersServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class RestUsersController {
     @Autowired
     UsersServices usersServices;
+
+    @PostMapping(value = "/api/create", headers = "Accept=application/json")
+    public void create(@RequestBody Users users) {
+        usersServices.create(users);
+    }
 
 
 }
