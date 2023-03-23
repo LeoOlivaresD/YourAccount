e2ce6601463e7e2f271084d96406547ddc927d8d
LeoOlivaresD <cspraword@gmail.com>
2023-03-23 02:52:26 -0300

    Rest Controller creacion de método delete

--- src/main/java/cl/maximuscode/youraccont/controllers/RestUsersController.java
+++ src/main/java/cl/maximuscode/youraccont/controllers/RestUsersController.java
@@ -1,11 +1,8 @@
 package cl.maximuscode.youraccont.controllers;
-
 import cl.maximuscode.youraccont.models.entities.Users;
 import cl.maximuscode.youraccont.models.service.UsersServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,12 +20,19 @@ public class RestUsersController {
     public List<Users> getUsers() {
         return usersServices.readAll();
     }
+
     @GetMapping(value = "/api/readOne/{id}")
-    public Optional<Users> readOne(@PathVariable("id")Integer idUser){
+    public Optional<Users> readOne(@PathVariable("id") Integer idUser) {
         return usersServices.readOne(idUser);
     }
+
     @PutMapping(value = "/api/update")
-    public void update(@RequestBody Users users){
+    public void update(@RequestBody Users users) {
         usersServices.update(users);
+    }
+
+    @DeleteMapping(value = "/api/delete/{id}")
+    public void delete(@PathVariable("id") Integer idUser) {
+        usersServices.delete(idUser);
     }
 }
